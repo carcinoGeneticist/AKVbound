98d96d15afaef1859b5d0b2b3b8d2c5d6ba2cb96
twinArmageddons <aisvyyn@Andrews-MacBook-Pro.local>
2014-03-11 23:38:04 -0400

    lab1 stairs
    also walk speed

--- src/Character.js
+++ src/Character.js
@@ -9,7 +9,7 @@ var Sburb = (function(Sburb){
 Sburb.Character = function(name,x,y,width,height,sx,sy,sWidth,sHeight,sheet,bootstrap){
 	Sburb.Sprite.call(this,name,x,y,width,height,null,null,Sburb.Sprite.prototype.MG_DEPTHING,true);
 
-	this.speed = 20;
+	this.speed = 7;
 	this.vx = 0;
 	this.vy = 0;
 	this.facing = "Front";
